3d698b028c40013cef26125638f457a82aab20c9
Sean R <fancysaurus@gmail.com>
2018-01-22 17:28:02 -0500

    First Main Commit of the Modoff!

---
+++ src/main/java/com/fancysaurus/arcanearomatics/common/ArcaneAromatics.java
@@ -0,0 +1,20 @@
+package com.fancysaurus.arcanearomatics.common;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ArcaneAromatics.MODID, version = ArcaneAromatics.VERSION)
+public class ArcaneAromatics
+{
+    public static final String MODID = "arcaro";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+}
