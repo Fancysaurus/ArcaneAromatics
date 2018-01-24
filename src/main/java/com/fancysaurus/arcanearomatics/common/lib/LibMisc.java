8e87d6ce59188a59c8f33ba656621b43af3faec7
Sean R <Fancysaurus@gmail.com>
2018-01-24 04:58:46 -0500

    Created Asset Folder Structure

--- src/main/java/com/fancysaurus/arcanearomatics/common/lib/LibMisc.java
+++ src/main/java/com/fancysaurus/arcanearomatics/common/lib/LibMisc.java
@@ -5,7 +5,7 @@ public final class LibMisc
     //Mod Constants
     public static final String MOD_ID = "arcaro";
     public static final String MOD_NAME = "Arcane Aromatics";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.1";
 
     //Proxy Constants
     public static final String PROXY_COMMON = "com.fancysaurus.arcanearomatics.common.core.proxy.CommonProxy";
